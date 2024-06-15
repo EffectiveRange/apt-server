b419cf22530e692aab1c16dfbd3dfcfa957f7c30
GitHub Action <action@github.com>
2024-06-15 14:06:29 +0000

    Update version to 1.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='apt-server',
-    version='1.1.3',
+    version='1.1.4',
     description='APT server with dynamic package pool handling',
     author='Ferenc Nandor Janky & Attila Gombos',
     author_email='info@effective-range.com',
